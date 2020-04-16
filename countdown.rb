58e332df136209a123c86e077e4ccf79103305b2
Abel Gechure <aoroko09@gmail.com>
2020-04-16 18:16:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,7 @@
 #write your code here
 
   def countdown(int)
+<<<<<<< HEAD
     while int > 0
       puts "#{int} SECOND(S)!"
       int -= 1
@@ -17,6 +18,21 @@
       "HAPPY NEW YEAR!"
    
   end  
+=======
+    while int > 0 do
+      puts "#{int} SECOND(S)!"
+      int -= 1
+   
+      
+    
+      # if int == 0
+      # return "HAPPY NEW YEAR!"
+      # end
+      
+    end
+  end
+  
+>>>>>>> 26690df8d3f01f7b5b8589f3cb77e83eb41a7780
  countdown(10) 
  
 
